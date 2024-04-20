67112ca912e153dc4a9ea95f8a07f85f0216da78
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-04-19 23:10:39 -0600

    Se crea el modelo de user

---
+++ src/user/user.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from 'mongoose';
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['USER_ROLE', 'PLATAFORM_ADMIN_ROLE', 'HOTEL_ADMIN_ROLE'],
+        default: 'USER_ROLE'
+    },
+});
+
+UserSchema.methods.toJSON = function () {
+    const { __v, password, _id, ...usuario } = this.toObject();
+    usuario.uid = _id;
+    return usuario;
+}
+
+export default mongoose.model('User', UserSchema);
