33fba9577a39eafbb56db6e8f41c75b1ce388ece
Epereira2021576 <134961772+Epereira2021576@users.noreply.github.com>
2024-04-30 23:30:23 -0600

    Added some fixes
    Added some fixes to the register function to make it match the parameters in the frontend form

--- src/middlewares/validar-jwt.js
+++ src/middlewares/validar-jwt.js
@@ -1,20 +1,20 @@
 import jwt from 'jsonwebtoken';
 
-export const validarJWT = (req, res, next) => {
+export const validarJWT = ( req, res, next ) => {
     let token = req.body.token || req.query.token || req.headers['authorization']
 
-    if (!token) {
-        return res.status(401).send('A token is required for authentication')
+    if ( !token ) {
+        return res.status( 401 ).send( 'A token is required for authentication' )
     }
 
     try {
-        token = token.replace(/^Bearer\s+/, '')
-        const decoded = jwt.verify(token, process.env.TOKEN_KEY)
+        token = token.replace( /^Bearer\s+/, '' )
+        const decoded = jwt.verify( token, process.env.TOKEN_KEY )
 
         req.user = decoded
-    } catch (e) {
-        console.log(e)
-        return res.status(401).send('Invalid Token')
+    } catch ( e ) {
+        console.log( e )
+        return res.status( 401 ).send( 'Invalid Token' )
     }
 
     return next()
