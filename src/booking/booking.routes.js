ca6b017885d5241dee439fdfb748518c2e20e9cf
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-05-22 05:07:19 -0600

    Se modifica el routes de Booking

--- src/booking/booking.routes.js
+++ src/booking/booking.routes.js
@@ -1,13 +1,15 @@
-import {Router} from 'express';
-import {createBooking, getAllBookings, getAllBookingsByUser} from './booking.controller.js';
-import {check} from 'express-validator';
-import {validarCampos} from '../middlewares/validar-campos.js';
-import {validarJWT} from '../middlewares/validar-jwt.js';
+import { Router } from 'express';
+import { createBooking, getAllBookings, getAllBookingsByUser } from './booking.controller.js';
+import { check } from 'express-validator';
+import { validarCampos } from '../middlewares/validar-campos.js';
+import { validarJWT } from '../middlewares/validar-jwt.js';
+import { hasUser } from '../helpers/role-verifiers.js'
 
 const router = Router();
 
 router.post('/add', [
     validarJWT,
+    hasUser,
     check('room', 'Room is required').not().isEmpty(),
     check('checkInDate', 'CheckInDate is required').not().isEmpty(),
     check('checkOutDate', 'CheckOutDate is required').not().isEmpty(),
@@ -15,7 +17,7 @@ router.post('/add', [
     validarCampos
 ], createBooking);
 
-router.get('/', validarJWT, getAllBookings);
+router.get('/', getAllBookings);
 
 router.get('/:id', validarJWT, getAllBookingsByUser);
 
