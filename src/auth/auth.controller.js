cd41b7f543f90984288a290acdc559ef47377db0
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-05-22 09:31:48 -0600

    Se documenta el auth controller

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -2,149 +2,128 @@ import User from '../user/user.model.js';
 import bcrypt from 'bcryptjs';
 import { generarJWT } from '../helpers/generar-jwt.js';
 
-
-// Register a new user
-export const register = async ( req, res ) => {
+// Registrar un nuevo usuario
+export const register = async (req, res) => {
     try {
         const { name, lastName, username, password, email } = req.body;
+        
+        // Generar una sal para encriptar la contraseña
         const salt = bcrypt.genSaltSync();
-        const encryptedPassword = bcrypt.hashSync( password, salt );
-        const encryptedAdminPass = bcrypt.hashSync( 'admin657', salt );
+        const encryptedPassword = bcrypt.hashSync(password, salt);
+        const encryptedAdminPass = bcrypt.hashSync('admin657', salt); // Contraseña para el administrador por defecto
 
-        // check if the user is trying to register with the Admin data
-        if ( req.body.name == 'Admin' || req.body.lastName == 'Admin' || req.body.username == 'admin' || req.body.email == 'admin@admin.com' ) {
-            return res.status( 400 ).json( {
-                msg: 'You cannot register with this data'
-            } );
+        // Verificar si el usuario intenta registrarse con datos de Admin
+        if (name === 'Admin' || lastName === 'Admin' || username === 'admin' || email === 'admin@admin.com') {
+            return res.status(400).json({ msg: 'No puedes registrarte con estos datos' });
         }
 
-        //if the email includes @hotelmanager.com, assign the HOTEL_ADMIN_ROLE to the user
-        if ( email.includes( '@hotelmanager.com' ) && req.headers['admin'] === 'PLATAFORM_ADMIN_ROLE' ) {
-            const user = await User.create( {
-                name,
-                lastName,
-                username,
-                email,
-                password: encryptedPassword,
-                role: 'HOTEL_ADMIN_ROLE'
-            } );
-            //return the userDetails
-            return res.status( 200 ).json( {
-                msg: "user has been added to database",
+        // Asignar rol HOTEL_ADMIN_ROLE si el email pertenece a hotelmanager.com
+        if (email.includes('@hotelmanager.com') && req.headers['admin'] === 'PLATAFORM_ADMIN_ROLE') {
+            const user = await User.create({
+                name, lastName, username, email, password: encryptedPassword, role: 'HOTEL_ADMIN_ROLE'
+            });
+            return res.status(200).json({
+                msg: "Usuario añadido a la base de datos",
                 userDetails: {
-                    name: user.name + " " + user.lastName,
+                    name: `${user.name} ${user.lastName}`,
                     username: user.username,
                     email: user.email,
-                },
-            } );
+                }
+            });
         }
-        const user = await User.create( {
-            name,
-            lastName,
-            username,
-            email,
-            password: encryptedPassword
-        } );
 
-        const findHotelAdmin = await User.findOne( { role: 'PLATAFORM_ADMIN_ROLE' } );
-        if ( !findHotelAdmin ) {
-            await User.create( {
+        // Crear un usuario normal
+        const user = await User.create({ name, lastName, username, email, password: encryptedPassword });
+
+        // Crear administrador por defecto si no existe
+        const findHotelAdmin = await User.findOne({ role: 'PLATAFORM_ADMIN_ROLE' });
+        if (!findHotelAdmin) {
+            await User.create({
                 name: 'Admin',
                 lastName: 'Admin',
                 username: 'admin',
                 email: 'admin@admin.com',
                 password: encryptedAdminPass,
                 role: 'PLATAFORM_ADMIN_ROLE'
-            } );
+            });
         }
-        //return the userDetails
-        return res.status( 200 ).json( {
-            msg: "user has been added to database",
+
+        return res.status(200).json({
+            msg: "Usuario añadido a la base de datos",
             userDetails: {
-                name: user.name + " " + user.lastName,
+                name: `${user.name} ${user.lastName}`,
                 username: user.username,
                 email: user.email,
-            },
-        } );
-    } catch ( error ) {
-        console.log( error );
-        res.status( 500 ).json( {
-            msg: 'Error registering user'
-        } );
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'Error al registrar el usuario' });
     }
-
 }
 
-//login a user
-export const login = async ( req, res ) => {
+// Iniciar sesión de un usuario
+export const login = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const userFound = await User.findOne( { email: email } );
-        const validPass = bcrypt.compareSync( password, userFound.password );
-
-        //if the user is found and the password is valid, generate a JWT
-        if ( userFound && validPass ) {
-            const token = await generarJWT( userFound.id, userFound.name, userFound.role );
-
-            return res.status( 200 ).json( {
-                msg: 'User logged in!',
-                userDetails: {
-                    name: userFound.name,
-                    email: userFound.email,
-                    role: userFound.role
-                },
-                token
-            } );
+        // Buscar usuario por email
+        const userFound = await User.findOne({ email });
+        if (!userFound) {
+            return res.status(400).send('Usuario no encontrado');
         }
 
-        if ( !userFound ) {
-            return res.status( 400 ).send(
-                'User not found'
-            );
+        // Verificar la contraseña
+        const validPass = bcrypt.compareSync(password, userFound.password);
+        if (!validPass) {
+            return res.status(400).send('Contraseña inválida');
         }
 
-        if ( !validPass ) {
-            return res.status( 400 ).send(
-                'Invalid password'
-            );
-        }
-
-    } catch ( e ) {
-        console.log( e );
-        res.status( 500 ).json( {
-            msg: 'Error when logging in'
-        } );
+        // Generar JWT si las credenciales son correctas
+        const token = await generarJWT(userFound.id, userFound.name, userFound.role);
+        return res.status(200).json({
+            msg: 'Usuario autenticado',
+            userDetails: {
+                name: userFound.name,
+                email: userFound.email,
+                role: userFound.role
+            },
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'Error al iniciar sesión' });
     }
 }
 
-export const registerPlataformAdmins = async ( req, res ) => {
+// Registrar administradores de la plataforma
+export const registerPlataformAdmins = async (req, res) => {
     try {
         const { name, lastName, username, password } = req.body;
-        const salt = bcrypt.genSaltSync();
-        const encryptedPassword = bcrypt.hashSync( password, salt );
 
-        //Create a user with the email being the combination of the name + lastName in uppercase + @admin.com
-        const user = await User.create( {
+        // Generar una sal y encriptar la contraseña
+        const salt = bcrypt.genSaltSync();
+        const encryptedPassword = bcrypt.hashSync(password, salt);
+
+        // Crear un usuario con el correo formado por nombre + apellido en mayúsculas + @admin.com
+        const user = await User.create({
             name,
             lastName,
             username,
-            email: ( name + lastName ).toUpperCase() + '@admin.com',
+            email: `${name}${lastName}`.toUpperCase() + '@admin.com',
             password: encryptedPassword,
             role: 'PLATAFORM_ADMIN_ROLE'
-        } );
+        });
 
-        return res.status( 200 ).json( {
-            msg: "user has been added to database",
+        return res.status(200).json({
+            msg: "Administrador añadido a la base de datos",
             userDetails: {
-                name: user.name + " " + user.lastName,
+                name: `${user.name} ${user.lastName}`,
                 username: user.username,
                 email: user.email,
-            },
-        } );
-    } catch ( e ) {
-        console.log( e );
-        res.status( 500 ).json( {
-            msg: 'Error when registering plataform admins'
-        } );
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'Error al registrar administradores de la plataforma' });
     }
 }
