1f780c31622a791d5f86ef0a15e28719cd90d31d
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-05-22 04:59:02 -0600

    Se modifica el routes de Hotel

--- src/hotel/hotel.routes.js
+++ src/hotel/hotel.routes.js
@@ -1,43 +1,44 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
 import { validarCampos } from '../middlewares/validar-campos.js';
-import { postHotel, getHotel, updateHotel, deleteHotel } from './hotel.controller.js';
+import { postHotel, getHotel, putHotel, deleteHotel } from './hotel.controller.js';
 import { validarJWT } from '../middlewares/validar-jwt.js';
+import { hasPlataformAdmin } from '../helpers/role-verifiers.js'
 
 const router = Router();
 
-router.post('/', [
+router.post('/add', [
     validarJWT,
+    hasPlataformAdmin,
     check('name', 'Name is required').not().isEmpty(),
     check('description', 'Description is required').not().isEmpty(),
     check('address', 'Address is required').not().isEmpty(),
     check('phone', 'Phone is required').not().isEmpty(),
     check('category', 'Category is required').not().isEmpty(),
-    check('pricePerNight', 'Price per night is required').not().isEmpty(),
     check('amenities', 'Amenities is required').not().isEmpty(),
     check('owner', 'Owner is required').not().isEmpty(),
     validarCampos
 ], postHotel);
 
 router.get('/', [
-    validarJWT
 ], getHotel);
 
-router.put('/:id', [
+router.put('/edit/:id', [
     validarJWT,
+    hasPlataformAdmin,
     check('name', 'Name is required').not().isEmpty(),
     check('description', 'Description is required').not().isEmpty(),
     check('address', 'Address is required').not().isEmpty(),
     check('phone', 'Phone is required').not().isEmpty(),
     check('category', 'Category is required').not().isEmpty(),
-    check('pricePerNight', 'Price per night is required').not().isEmpty(),
     check('amenities', 'Amenities is required').not().isEmpty(),
     check('owner', 'Owner is required').not().isEmpty(),
     validarCampos
-], updateHotel);
+], putHotel);
 
-router.delete('/:id', [
-    validarJWT
-],deleteHotel);
+router.delete('/delete/:id', [
+    validarJWT,
+    hasPlataformAdmin
+], deleteHotel);
 
 export default router;
