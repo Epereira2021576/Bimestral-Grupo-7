bb9389cbfb5b24bd90f463fbefa5f434aa587b84
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-05-22 09:42:13 -0600

    Se documentan los helpers

--- src/helpers/db-validators.js
+++ src/helpers/db-validators.js
@@ -1,23 +1,26 @@
-import User from '../user/user.model.js'
-import Room from '../room/room.model.js'
+import User from '../user/user.model.js';
+import Room from '../room/room.model.js';
 
+// Verificar si ya existe un usuario con el mismo email
 export const existeEmail = async (email = '') => {
-    const existe = await User.findOne({ email })
+    const existe = await User.findOne({ email });
     if (existe) {
-        throw new Error(`El email ${email} ya fue registrado`)
+        throw new Error(`El email ${email} ya fue registrado`);
     }
-}
+};
 
+// Verificar si existe un usuario con el ID especificado
 export const existeUsuarioById = async (id = '') => {
-    const existeUsuario = await User.findById(id)
+    const existeUsuario = await User.findById(id);
     if (!existeUsuario) {
-        throw new Error(` el ID: ${id} no existe`)
+        throw new Error(`El usuario con ID: ${id} no existe`);
     }
-}
+};
 
+// Verificar si existe una habitación con el ID especificado
 export const existeHabitacionById = async (id = '') => {
     const existeHabitacion = await Room.findById(id);
     if (!existeHabitacion) {
-        throw new Error(`Room with ID: ${id} don't exists`);
+        throw new Error(`La habitación con ID: ${id} no existe`);
     }
-}
+};
--- src/helpers/role-verifiers.js
+++ src/helpers/role-verifiers.js
@@ -1,58 +1,75 @@
-import User from '../user/user.model.js'
+import User from '../user/user.model.js';
 
-
+// Middleware para verificar si el usuario tiene el rol de administrador de plataforma
 export const hasPlataformAdmin = async (req, res, next) => {
     try {
         const userID = req.headers['authorized'];
         const checkUser = await User.findById(userID);
+        
+        // Verificar si el usuario existe y tiene el rol de administrador de plataforma
         if (!checkUser || checkUser.role !== 'PLATAFORM_ADMIN_ROLE') {
             return res.status(401).json({
-                msg: 'Unauthorized'
+                msg: 'No autorizado'
             });
         }
-        next(); // Proceed to the next middleware
+        
+        // Si pasa la verificación, pasar al siguiente middleware
+        next();
     } catch (error) {
+        // Manejar errores y responder con un mensaje de error
         console.error(error);
         res.status(500).json({
-            msg: 'Failed to verify user role.',
+            msg: 'Error al verificar el rol del usuario.',
             error: error.message
         });
     }
 };
 
+// Middleware para verificar si el usuario tiene el rol de administrador de hotel
 export const hasHotelAdmin = async (req, res, next) => {
     try {
         const userID = req.headers['authorized'];
         const checkUser = await User.findById(userID);
+        
+        // Verificar si el usuario existe y tiene el rol de administrador de hotel
         if (!checkUser || checkUser.role !== 'HOTEL_ADMIN_ROLE') {
             return res.status(401).json({
-                msg: 'Unauthorized'
+                msg: 'No autorizado'
             });
         }
+        
+        // Si pasa la verificación, pasar al siguiente middleware
         next();
     } catch (error) {
+        // Manejar errores y responder con un mensaje de error
         console.error(error);
         res.status(500).json({
-            msg: 'Failed to verify user role.',
+            msg: 'Error al verificar el rol del usuario.',
             error: error.message
         });
     }
 };
 
+// Middleware para verificar si el usuario tiene el rol de usuario regular
 export const hasUser = async (req, res, next) => {
     try {
         const userID = req.headers['authorized'];
         const checkUser = await User.findById(userID);
+        
+        // Verificar si el usuario existe y tiene el rol de usuario regular
         if (!checkUser || checkUser.role !== 'USER_ROLE') {
             return res.status(401).json({
-                msg: 'Unauthorized'
+                msg: 'No autorizado'
             });
         }
+        
+        // Si pasa la verificación, pasar al siguiente middleware
         next();
     } catch (error) {
+        // Manejar errores y responder con un mensaje de error
         console.error(error);
         res.status(500).json({
-            msg: 'Failed to verify user role.',
+            msg: 'Error al verificar el rol del usuario.',
             error: error.message
         });
     }
